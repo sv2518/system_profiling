164a853d197a905b6cbf1bb1c7be70ee55d3adab
Jack Betteridge <j.betteridge@imperial.ac.uk>
2021-03-18 17:12:24 +0000

    Fixed for Koki's workstation

--- stream/plot_stream.py
+++ stream/plot_stream.py
@@ -4,10 +4,13 @@ import matplotlib.pyplot as plt
 from argparse import ArgumentParser
 from pathlib import Path
 from pickle import load
+from run_stream import size2val
 
 parser = ArgumentParser()
+parser.add_argument('--channels', type=int, default=None)
 parser.add_argument('-i', '--input', default='results.pickle')
 parser.add_argument('-o', '--output')
+parser.add_argument('--single_channel', type=str, default=None)
 parser.add_argument('-t', '--title', type=str, default=None)
 args, _ = parser.parse_known_args()
 
@@ -15,19 +18,32 @@ inputfile = Path(args.input).absolute()
 with open(inputfile, 'rb') as fh:
     results = load(fh)
 
-cores = [ii +1 for ii in range(len(results))]
+cores = [ii + 1 for ii in range(len(results))]
+if args.channels:
+    channels = [ii + 1 for ii in range(args.channels)]
+else:
+    channels = [ii + 1 for ii in range(len(results))]
+if args.single_channel:
+    single_channel = size2val(args.single_channel)
+else:
+    single_channel = results[0]
+
 
 DPI = 300
 fig, ax = plt.subplots(1, 1)
 fig.set_size_inches((8, 8))
 
-ax.plot(results, 'x-')
+perfect = [c*single_channel/(2**30) for c in channels]
+# ~ ax.plot([0], [0], 'ko')
+ax.plot(channels, perfect, 'k:')
+ax.plot([channels[-1], cores[-1]], [perfect[-1], perfect[-1]], 'k:')
+ax.plot(cores, results/(2**10), 'x-')
 if args.title:
     ax.set_title(str(args.title))
 else:
     ax.set_title('Streams')
 ax.set_xlabel('Cores')
-ax.set_ylabel('Rate MB/s')
+ax.set_ylabel('Rate GB/s')
 
 if args.output:
     plt_name = Path(args.output).absolute()
--- stream/run_stream.py
+++ stream/run_stream.py
@@ -4,10 +4,23 @@ from argparse import ArgumentParser
 from os import environ
 from pathlib import Path
 from pickle import dump
-from subprocess import run, PIPE
+from subprocess import run, PIPE, CalledProcessError
 
 def guess_cores():
     return psutil.cpu_count(logical=False)
+
+
+def size2val(size):
+    if size.isdecimal():
+        value = int(size)
+    else:
+        suffix = ['', 'k', 'm', 'g']
+        *val, unit = size
+        try:
+            value = int(''.join(val)) * 2**(10*suffix.index(unit.lower()))
+        except ValueError:
+            value = float(''.join(val)) * 2**(10*suffix.index(unit.lower()))
+    return value
 
 
 def guess_l3():
@@ -18,72 +31,71 @@ def guess_l3():
                 cache = s
                 break
         size = cache.split()[2]
-        if size.isdecimal():
-            l3 = int(size)
-        else:
-            suffix = ['', 'k', 'm', 'g']
-            *val, unit = size
-            l3 = int(''.join(val)) * 2**(10*suffix.index(unit.lower()))
+        l3 = units2val(size)
     except FileNotFoundError:
-        print('No command `lscpu` using default value of 20MB for L3 cache size')
+        print('No command `lscpu`, using default value of 20MB for L3 cache size')
+        l3 = 20*2**20
+    except CalledProcessError:
+        print('`lscpu` doesn\'t support `-c` flag, using default value of 20MB for L3 cache size')
         l3 = 20*2**20
     return l3
 
 
-# Parse command line arguments
-parser = ArgumentParser()
-parser.add_argument('-c', '--cores', type=int, default=None)
-parser.add_argument('--cc', type=str, default='gcc')
-parser.add_argument('-l3', '--l3', type=int, default=None)
-parser.add_argument('-o', '--output', default='results.pickle')
-parser.add_argument('--offset', type=int, default=0)
-parser.add_argument('-r', '--repeats', type=int, default=10)
-args, _ = parser.parse_known_args()
+if __name__ == '__main__':
+    # Parse command line arguments
+    parser = ArgumentParser()
+    parser.add_argument('-c', '--cores', type=int, default=None)
+    parser.add_argument('--cc', type=str, default='gcc')
+    parser.add_argument('-l3', '--l3', type=str, default=None)
+    parser.add_argument('-o', '--output', default='results.pickle')
+    parser.add_argument('--offset', type=int, default=0)
+    parser.add_argument('-r', '--repeats', type=int, default=10)
+    args, _ = parser.parse_known_args()
 
-if args.cores:
-    cores = args.cores
-else:
-    cores = guess_cores()
+    if args.cores:
+        cores = args.cores
+    else:
+        cores = guess_cores()
 
-if args.l3:
-    l3 = args.l3
-else:
-    l3 = guess_l3()
+    if args.l3:
+        l3 = size2val(args.l3)
+    else:
+        l3 = guess_l3()
 
-# L3 cache multiplied by 4, then divided by sizeof(double) = 8
-array_size = l3//2
+    # L3 cache multiplied by 4, then divided by sizeof(double) = 8
+    array_size = l3//2
 
-compiler = args.cc
-cflags = '-march=native -O3 -fopenmp -ffast-math'
-pp_defs = f'-DSTREAM_ARRAY_SIZE={array_size} -DNTIMES={args.repeats} -DOFFSET={args.offset}'
-source_file = Path('stream.c').absolute()
-executable_file = source_file.with_suffix('')
+    compiler = args.cc
+    cflags = '-march=native -O3 -fopenmp -ffast-math'
+    pp_defs = f'-DSTREAM_ARRAY_SIZE={array_size} -DNTIMES={args.repeats} -DOFFSET={args.offset}'
+    source_file = Path('stream.c').absolute()
+    executable_file = source_file.with_suffix('')
 
-compile_command = [compiler] + cflags.split() + pp_defs.split()
-compile_command += ['-o', str(executable_file)]
-compile_command.append(str(source_file))
+    compile_command = [compiler] + cflags.split() + pp_defs.split()
+    compile_command += ['-o', str(executable_file)]
+    compile_command.append(str(source_file))
 
-# ~ print(compile_command)
-run(compile_command)
+    # ~ print(compile_command)
+    run(compile_command)
 
-run_env = environ.copy()
-run_env['OMP_DISPLAY_AFFINITY'] = 'TRUE'
-run_env['OMP_PLACES'] = 'cores'
-run_env['OMP_PROC_BIND'] = 'spread'
-results = []
-for ii in range(cores):
-    run_env['OMP_NUM_THREADS'] = str(ii + 1)
-    output = run([executable_file], env=run_env, check=True, stdout=PIPE, stderr=PIPE, encoding='UTF-8')
-    for line in output.stdout.split('\n'):
-        # ~ print(line)
-        if line.startswith('level'):
-            print(line)
-        elif line.startswith('Triad'):
-            speed = line.split()[1]
-    results.append(speed)
-    print('Cores:', ii + 1, 'Rate:', speed, 'MB/s')
-    print('Affinity:\n', output.stderr)
+    run_env = environ.copy()
+    run_env['OMP_DISPLAY_AFFINITY'] = 'TRUE'
+    run_env['OMP_PLACES'] = 'cores'
+    run_env['OMP_PROC_BIND'] = 'spread'
+    results = []
+    for ii in range(cores):
+        run_env['OMP_NUM_THREADS'] = str(ii + 1)
+        output = run([executable_file], env=run_env, check=True, stdout=PIPE, stderr=PIPE, encoding='UTF-8')
+        for line in output.stdout.split('\n'):
+            # ~ print(line)
+            if line.startswith('level'):
+                print(line)
+            elif line.startswith('Triad'):
+                speed = line.split()[1]
+        results.append(float(speed))
+        print('Cores:', ii + 1, 'Rate:', speed, 'MB/s')
+        print('Affinity:\n', output.stderr)
 
-outfile = Path(args.output).absolute()
-with open(outfile, 'wb') as fh:
-    dump(results, fh)
+    outfile = Path(args.output).absolute()
+    with open(outfile, 'wb') as fh:
+        dump(results, fh)
