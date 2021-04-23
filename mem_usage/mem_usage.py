b7b462ef94d800825f6eb974ffe915a2bf215946
JDBetteridge <J.Betteridge@imperial.ac.uk>
2021-04-23 15:35:41 +0100

    Added more options to mem_usage

--- mem_usage/mem_usage.py
+++ mem_usage/mem_usage.py
@@ -4,6 +4,7 @@ import time
 
 from argparse import ArgumentParser
 from csv import DictWriter
+from pathlib import Path
 
 parser = ArgumentParser()
 subparsers = parser.add_subparsers(dest='command')
@@ -11,8 +12,9 @@ mon_parser = subparsers.add_parser('monitor')
 mon_parser.add_argument('--target', type=str, help='Name of target application')
 mon_parser.add_argument('--timeout', type=int, default=10, help='Time to wait for application to start')
 mon_parser.add_argument('--free', action='store_true', help='Monitor remaining memory')
+mon_parser.add_argument('--output', type=str, help='Name to save log to')
 plot_parser = subparsers.add_parser('plot')
-plot_parser.add_argument('--input', type=str, help='CSV to read')
+plot_parser.add_argument('--input', type=str, nargs='+', help='CSV to read')
 plot_parser.add_argument('--output', type=str, help='Name to save plot to')
 args, unknown = parser.parse_known_args()
 
@@ -20,7 +22,7 @@ def monitor(args):
     import psutil
     try:
         from mpi4py import MPI
-        rank = MPI.rank
+        rank = MPI.COMM_WORLD.rank
     except ImportError:
         rank = 0
 
@@ -38,7 +40,11 @@ def monitor(args):
         raise ProcessLookupError(f'Target application did not start within {args.timeout} seconds of this script launching')
 
     mem = psutil.virtual_memory()._asdict()
-    with open(f'free{rank}_{int(time.time())}.csv', 'w') as fh:
+    if args.output:
+        outfile = f'{args.output}_{rank}.csv'
+    else:
+        outfile = f'free{rank}_{int(time.time())}.csv'
+    with open(outfile, 'w') as fh:
         start = time.time()
         csv = DictWriter(fh, ['time'] + [k for k in mem.keys()])
         csv.writeheader()
@@ -54,24 +60,29 @@ def plot(args):
     from pandas import read_csv
 
     if args.input is None:
-        infile = sorted(filter(lambda x: x.startswith('free') and x.endswith('.csv'), os.listdir()))[-1]
+        infile = [sorted(filter(lambda x: x.startswith('free') and x.endswith('.csv'), os.listdir()))[-1]]
     else:
         infile = args.input
-    data = read_csv(infile)
-    data['available (GB)'] = data['available']/(1024**3)
 
-    fig, ax = plt.subplots(1, 1)
-    fig.set_size_inches(8, 6)
-    data.plot('time', 'available (GB)', ax=ax)
+    for ii, csv in enumerate(infile):
+        data = read_csv(csv)
+        data['available (GB)'] = data['available']/(1024**3)
 
-    ax.set_xlabel('Time (s)')
-    ax.set_ylim(0, data['total'][0]/(1024**3))
-    ax.set_ylabel('Free Memory (GB)')
+        fig, ax = plt.subplots(1, 1)
+        fig.set_size_inches(8, 6)
+        data.plot('time', 'available (GB)', ax=ax)
 
-    if args.output:
-        fig.savefig(args.output)
-    else:
-        plt.show()
+        ax.set_xlabel('Time (s)')
+        ax.set_ylim(0, data['total'][0]/(1024**3))
+        ax.set_ylabel('Free Memory (GB)')
+
+        if args.output:
+            outfile = Path(args.output)
+            if len(infile) > 1:
+                outfile = outfile.with_stem(outfile.stem + f'_{ii}')
+            fig.savefig(outfile)
+        else:
+            fig.savefig(Path(csv).with_suffix('.png'))
 
 if __name__ == '__main__':
     if args.command == 'monitor':
